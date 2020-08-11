4210a2492c9321356278c9e282932b821e815df3
Cong Vu <congvv@imt-soft.com>
2020-08-11 15:44:09 +0700

    creat new code

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use('/public', express.static(path.join(__dirname,'/public')))
+
+app.get ('/', (req, res, next) => {
+    var duongDanFile = path.join(__dirname, './home.html')
+    res.sendFile(duongDanFile);
+})
+
+app.listen(3000, () => {
+    console.log('Server is starting');
+})
